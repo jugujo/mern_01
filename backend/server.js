dc3b07bc0d03da851fc618ff0c196c8f933920a9
김주환 <gimjuhwan@gimjuhwan-ui-Macmini.local>
2024-03-17 21:00:03 +0900

    feat: react console by express response

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,82 @@
-const express = require('express')
-const connectDB = require('./config/db');
+// const express = require('express')
+// const connectDB = require('./config/db');
 
-const app = express()
-connectDB();
+// const app = express()
+// connectDB();
 
-app.get('/', (req, res) => res.send('respooo'));
+// app.get('/', (req, res) => res.send('respooo'));
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server start on ${PORT}`));
+// app.listen(PORT, () => console.log(`Server start on ${PORT}`));
 
-// const User = require('./model/User')
+// // const User = require('./model/User')
 
-// let name = 'a';
-// let email = 'a';
-// let passworf = 'a';
+// // let name = 'a';
+// // let email = 'a';
+// // let passworf = 'a';
 
-// let user = new User({
-//     name,
-//     email,
-//     passworf
-// })
+// // let user = new User({
+// //     name,
+// //     email,
+// //     passworf
+// // })
 
-// Call start
-// (async() => {
-//     console.log('before start');
-  
-//     // let user = await User.findOne({ 'aa'})
-//     // user.name = 'name'
+// // Call start
+// // (async() => {
+// //     console.log('before start');
 
-//     await user.save();
-    
-//     console.log('after start');
-//   })();
+// //     // let user = await User.findOne({ 'aa'})
+// //     // user.name = 'name'
 
+// //     await user.save();
 
+// //     console.log('after start');
+// //   })();
 
+// node_modules 에 있는 express 관련 파일을 가져온다.
+const express = require("express");
+// const axios = require("axios");
+const cors = require("cors");
+const port = 5001;
+
+// app.listen(port, () => console.log("running"));
+// express 는 함수이므로, 반환값을 변수에 저장한다.
+var app = express();
+
+app.use(cors()); // 모든 도메인에서의 요청을 허용
+// 5001 포트로 서버 오픈
+app.listen(5001, function () {
+  console.log("start! express server on port 5001");
+});
+
+app.get("/", function (req, res) {
+  res.send({ message: "Hello world!" });
+});
+
+app.get("/main", function (req, res) {
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get("/test", function (req, res) {
+  res.send(
+    `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>main.html</title>
+</head>
+<body>
+    <h1>main page</h1>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Atque, ullam quos! Iste quae, molestiae vitae tenetur, 
+        in a aperiam voluptatibus dicta qui doloribus libero suscipit optio delectus voluptas voluptatem impedit!</p>
+</body>
+</html>
+`
+  );
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 const App = () => {
+    /**
+     * example express <-> react
+     * */
+    const url = 'http://localhost:5001/'
+    const api = async () => {
+        const response = await fetch(url)
+        const jsonData = await response.json()
+        console.log('#jsonData = ', jsonData)
+    }
+    api()
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
             </header>
         </div>
     )
