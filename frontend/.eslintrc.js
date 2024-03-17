f787ba169bfd1663bf6c6ab2a466facfe21dd274
김주환 <gimjuhwan@gimjuhwan-ui-Macmini.local>
2024-03-17 19:24:46 +0900

    feat: mod eslint error-storybook and etc[WIP]

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,14 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'react-app', 'plugin:prettier/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'react-app',
+        'plugin:prettier/recommended',
+        'plugin:storybook/recommended',
+        'plugin:storybook/recommended',
+    ],
     overrides: [
         {
             env: {
