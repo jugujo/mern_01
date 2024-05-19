8f66810d59bb9d3248d01a56ccf1a8c2ec746939
김주환 <gimjuhwan@gimjuhwan-ui-Macmini.local>
2024-05-19 23:01:25 +0900

    feat: PR backend, frontend

---
+++ frontend/src/components/pages/2_Body/MongoDB/Todo.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import TodoTable from './TodoTable'
+import TodoHeader from './TodoHeader'
+import { useTodoHooks } from '../../../../hooks/todos/TodoHooks'
+
+const Todo: React.FC = () => {
+    const {
+        todos,
+        username,
+        setUsername,
+        title,
+        setTitle,
+        contents,
+        setContents,
+        onHandleParam,
+        onInsertHandle,
+        onSelectRow,
+        onToggleEditMode,
+        onUpdateSelected,
+        onToggleSelectAll,
+        selectAll,
+        error,
+    } = useTodoHooks()
+
+    return (
+        <div>
+            <h1>Todos</h1>
+            {error && <div style={{ color: 'red' }}>{error}</div>}
+            <TodoHeader
+                username={username}
+                setUsername={setUsername}
+                title={title}
+                setTitle={setTitle}
+                contents={contents}
+                setContents={setContents}
+                onInsertHandle={onInsertHandle}
+                onUpdateSelected={onUpdateSelected}
+            />
+            <TodoTable
+                todos={todos}
+                onHandleParam={onHandleParam}
+                onSelectRow={onSelectRow}
+                onToggleEditMode={onToggleEditMode}
+                onToggleSelectAll={onToggleSelectAll}
+                selectAll={selectAll}
+            />
+        </div>
+    )
+}
+
+export default Todo
